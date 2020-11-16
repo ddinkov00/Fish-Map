e3a4a1f0ed9295d200676385be2c561e91b68cac
DDinkov00 <65896735+ddinkov00@users.noreply.github.com>
2020-11-16 13:44:43 +0200

    Revert "Revert "Database models created""
    This reverts commit 181994d94af1f644d3e5391d10f5721172ca5a8d.

---
+++ Data/FishMap.Data.Models/Location.cs
@@ -0,0 +1,27 @@
+﻿namespace FishMap.Data.Models
+{
+    using FishMap.Data.Common.Models;
+
+    public class Location : BaseDeletableModel<int>
+    {
+        public float Latitude { get; set; }
+
+        public float Longtitude { get; set; }
+
+        public bool? IsFishingSpot { get; set; }
+
+        public bool? IsMeetingSpot { get; set; }
+
+        public int? TripId { get; set; }
+
+        public virtual Trip Trip { get; set; }
+
+        public int? TownId { get; set; }
+
+        public virtual Town Town { get; set; }
+
+        public int? GroupTripId { get; set; }
+
+        public virtual GroupTrip GroupTrip { get; set; }
+    }
+}
