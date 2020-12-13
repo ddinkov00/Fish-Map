93ff12fd2e3005819c5e81c89bdbbc70b54a9fa7
DDinkov00 <65896735+ddinkov00@users.noreply.github.com>
2020-12-13 22:40:19 +0200

    All group trips action and required services created

---
+++ Services/FishMap.Services.Data/TownsService.cs
@@ -0,0 +1,47 @@
+﻿using FishMap.Data.Common.Repositories;
+using FishMap.Data.Models;
+using FishMap.Services.Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FishMap.Services.Data
+{
+    public class TownsService : ITownsService
+    {
+        private readonly IDeletableEntityRepository<Town> townsRepository;
+
+        public TownsService(IDeletableEntityRepository<Town> townsRepository)
+        {
+            this.townsRepository = townsRepository;
+        }
+
+        public string GetNearestCity(float latitude, float longtitude)
+        {
+            var minDistance = float.MaxValue;
+            var closestCityName = string.Empty;
+
+            var towns = this.townsRepository.AllAsNoTracking()
+                .Select(t => new
+                {
+                    Name = t.Name,
+                    Latitude = t.LocationLatitude,
+                    Longtitude = t.LocationLongtitude,
+                }).ToList();
+
+            foreach (var town in towns)
+            {
+                var distance = (float)Math.Sqrt(Math.Pow(latitude - town.Latitude, 2) + Math.Pow(longtitude - town.Longtitude, 2));
+
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closestCityName = town.Name;
+                }
+            }
+
+            return closestCityName;
+        }
+    }
+}
---
+++ Web/FishMap.Web.ViewModels/GroupTrips/GroupTripInListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FishMap.Web.ViewModels.GroupTrips
+{
+    class GroupTripInListViewModel
+    {
+    }
+}
